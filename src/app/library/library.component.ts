32ecd39a2958f282c966a32db9f418a769823c55
Mattie Schraeder <mattie@geekwaytothewest.com>
2019-10-29 20:29:55 -0500

    fully working library!

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -20,15 +20,23 @@ export class LibraryComponent implements OnInit {
   libraryData: any;
   libraryDataObservable: Observable<any>;  
   dataSource: any;
-  columnsToDisplay = ['Image', 'Name'];
+  columnsToDisplay = ['Image', 'Name', 'Year', 'Players', 'Playtime', 'Rating'];
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   nameFilter = new FormControl();
+  yearFilter = new FormControl();
+  playersFilter = new FormControl();
+  playtimeFilter = new FormControl();
+  ratingFilter = new FormControl();
 
   filterValues = {
-    name: ''
+    name: '',
+    year: '',
+    players: '',
+    playtime: '',
+    rating: ''
   };
 
   constructor(
@@ -45,7 +53,6 @@ export class LibraryComponent implements OnInit {
           let parser = new xml2js.Parser();
   
           parser.parseStringPromise(this.libraryXml).then((result) => {
-            console.log(result.items.item);
             observer.next(result.items.item);                        
             this.libraryData = result.items.item;
             observer.complete();
@@ -66,14 +73,62 @@ export class LibraryComponent implements OnInit {
       this.filterValues.name = name;
       this.dataSource.filter = JSON.stringify(this.filterValues);
     })
+
+    this.yearFilter.valueChanges.subscribe(year => {
+      this.filterValues.year = year;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    })
+
+    this.playersFilter.valueChanges.subscribe(players => {
+      this.filterValues.players = players;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    })
+
+    this.playtimeFilter.valueChanges.subscribe(playtime => {
+      this.filterValues.playtime = playtime;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    })
+
+    this.ratingFilter.valueChanges.subscribe(rating => {
+      this.filterValues.rating = rating;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    })
   }
 
   tableFilter(): (data: any, filter: string) => boolean {
     let filterFunction = function(data, filter): boolean {
       let searchTerms = JSON.parse(filter);
-      return data.name[0]._.toLowerCase().indexOf(searchTerms.name) !== -1
+
+      let nameFound = data.name[0]._.toLowerCase().indexOf(searchTerms.name) !== -1;
+      let yearFound = data.yearpublished ? data.yearpublished[0].indexOf(searchTerms.year) !== -1 : false;
+
+      let statsObject = data.stats[0].$;
+
+      let players = true;
+      let playtime = true;
+      let rating = true;
+
+      if (statsObject) {
+        if (!isNaN(parseInt(searchTerms.players))) {
+          players = parseInt(statsObject.minplayers) <= parseInt(searchTerms.players) && parseInt(statsObject.maxplayers) >= parseInt(searchTerms.players);
+        }
+
+        if (!isNaN(parseInt(searchTerms.playtime))) {
+          playtime = parseInt(statsObject.minplaytime) <= parseInt(searchTerms.playtime) && parseInt(statsObject.maxplaytime) >= parseInt(searchTerms.playtime);
+        }
+
+        if (!isNaN(parseFloat(searchTerms.rating))) {
+          rating = parseFloat(data.stats[0].rating[0].average[0].$.value) >= parseFloat(searchTerms.rating);
+        }
+      }
+
+      return nameFound && yearFound && players && playtime && rating;
     }
     return filterFunction;
   } 
+
+  bggRedirect(bggId: String) {
+    window.open("https://boardgamegeek.com/boardgame/" + bggId, '_blank');
+  }
 
 }
