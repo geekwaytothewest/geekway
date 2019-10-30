158b0cc45ee7dbc19b31970ecc6f87c094318685
Mattie Schraeder <mattie@geekwaytothewest.com>
2019-10-30 10:59:45 -0500

    Skeleton for conventions works!

--- src/app/conventions/conventions.module.ts
+++ src/app/conventions/conventions.module.ts
@@ -5,13 +5,27 @@ import { ConventionsComponent } from './conventions.component';
 import { MatTableModule } from '@angular/material';
 import { MomentModule } from 'ngx-moment';
 import { MomentTimezoneModule } from '../shared/moment-timezone/moment-timezone.module';
+import { ConventionComponent } from './convention/convention.component';
+import { GeekwaytothewestComponent } from './geekwaytothewest/geekwaytothewest.component';
+import { GeekwaymicroComponent } from './geekwaymicro/geekwaymicro.component';
+import { GeekwayminiComponent } from './geekwaymini/geekwaymini.component';
 
 const conventionsRoutes: Routes = [
-  { path: '', component: ConventionsComponent, pathMatch: 'full' }
+  { path: '', component: ConventionsComponent, pathMatch: 'full' },
+  { path: 'geekway', component: GeekwaytothewestComponent},
+  { path: 'mini', component: GeekwayminiComponent},
+  { path: 'micro', component: GeekwaymicroComponent},
+  { path: 'convention/:slug', component: ConventionComponent }
 ]
 
 @NgModule({
-  declarations: [ConventionsComponent],
+  declarations: [
+    ConventionsComponent, 
+    ConventionComponent, 
+    GeekwaytothewestComponent, 
+    GeekwayminiComponent, 
+    GeekwaymicroComponent
+  ],
   imports: [
     CommonModule,
     MatTableModule,
