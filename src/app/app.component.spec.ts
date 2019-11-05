0104cc844ddcbbe1b74b97e240d5eab9d4ec0d1a
Mattie Schraeder <mattie@geekwaytothewest.com>
2019-11-05 10:09:14 -0600

    another unit test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MatIconModule, MatCardModule } from '@angular/material';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let backend: ApolloTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule, MatIconModule, MatCardModule
+        RouterTestingModule, MatIconModule, MatCardModule, ApolloTestingModule
       ],
       declarations: [
         AppComponent, NavigationComponent
       ],
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    backend = TestBed.get(ApolloTestingController);
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
