e3e99751fb73c60c0998cb66bd480b3d708e9e84
Mattie Schraeder <mattie@mattie.lgbt>
2020-10-31 04:05:12 -0500

    Fix blog bugs

--- src/app/blogs/blogpost/blogpost.component.ts
+++ src/app/blogs/blogpost/blogpost.component.ts
@@ -43,7 +43,7 @@ export class BlogpostComponent implements OnInit {
     );
 
     this.blogPostSubscription = this.blogPost.subscribe(result => {
-      this.blogContent = this.sanitizer.bypassSecurityTrustHtml(result.content.replace('<oembed url=', ' <div class="iframely-embed"><div class="iframely-responsive"><a data-iframely-url href=') + '</div></div>');
+      this.blogContent = this.sanitizer.bypassSecurityTrustHtml(result.content.replace('<oembed url=', ' <div class="iframely-embed"><div class="iframely-responsive"><a data-iframely-url href=').replace('src="/uploads/', 'src="https://cms.geekway.com/uploads/') + '</div></div>');
       this.headerPhotos.announceHeaderLabelChanged(result.Title);
       this.headerPhotos.announceHeaderPhotoChanged("https://cms.geekway.com/" + result.HeaderPhoto.url);
     })
