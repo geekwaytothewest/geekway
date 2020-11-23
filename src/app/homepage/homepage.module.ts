a9468ff153f8c595fddf5fb535700c1667a7a000
Mattie Schraeder <mattie@mattie.lgbt>
2020-11-23 08:44:40 -0600

    tslinting

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -5,12 +5,12 @@ import { HomepageComponent } from './homepage.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MomentModule } from 'ngx-moment';
-import { MomentTimezoneModule } from '../shared/moment-timezone/moment-timezone.module'
+import { MomentTimezoneModule } from '../shared/moment-timezone/moment-timezone.module';
 import { FlipclockModule } from '../shared/flipclock/flipclock.module';
 
 const homepageRoutes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' }
-]
+];
 
 @NgModule({
   declarations: [
